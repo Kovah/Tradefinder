427d38834a50fe9ff8db926ec268ced7e8d3d47b
Kovah <mail@kovah.de>
2020-09-09 00:33:39 +0200

    Add header and footer to app layout

---
+++ components/Header.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Header = () => (
+  <h1 className={'text-2xl text-center uppercase tracking-wider'}>Tradefinder</h1>
+);
+
+export default Header;
--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Hello = () => (
-  <div>Hello World</div>
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+const App = () => (
+  <div className={'container mx-auto max-w-5xl py-8'}>
+    <Header/>
+    <Footer/>
+  </div>
 );
 
 ReactDOM.render(
-  <Hello/>,
+  <App/>,
   document.querySelector('#root')
 );
