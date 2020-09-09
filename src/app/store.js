d4cecb6ab1f74ad6a7328bfb14d0152a19816c96
Kovah <mail@kovah.de>
2020-09-09 18:43:13 +0200

    Remove testing slice for header

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import tradingReducer from '../features/trading/tradingSlice';
 import itemsReducer from '../features/items/itemsSlice';
 
 export default configureStore({
   reducer: {
-    trading: tradingReducer,
     items: itemsReducer,
   },
 });
--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,14 +1,7 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { increment, selectCount } from '../features/trading/tradingSlice';
 
 export function Header () {
-  const trading = useSelector(selectCount);
-  const dispatch = useDispatch();
-
   return (
-    <h1 className="text-2xl text-center uppercase tracking-widest" onClick={() => dispatch(increment())}>
-      Tradefinder <span>{trading.value}</span>
-    </h1>
+    <h1 className="text-2xl text-center uppercase tracking-widest">Tradefinder</h1>
   )
 }
