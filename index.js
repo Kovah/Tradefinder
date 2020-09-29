2893efead86a2f3b4e7fb550cda2c3c4f8b71101
Kovah <mail@kovah.de>
2020-09-30 01:55:31 +0200

    Update app version, slightly reduce text color brightness

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import App from './src/App';
 import store from './src/app/store';
 import { saveState } from './src/app/localstorage';
 
-export const appVersion = 1599727940;
+export const appVersion = 1601423695;
 
 store.subscribe(() => {
   saveState(store.getState());
